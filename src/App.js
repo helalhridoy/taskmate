33e05ca9e24704d44758a3832f2f6c20db99f6d4
helal <helal.h.hridoy@gmail.com>
2024-04-16 10:32:19 +0600

    App Starting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import Header from './components/Header';
+import Addtask from './components/Addtask';
+import Showtask from './components/Showtask';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Addtask></Addtask>
+      <Showtask></Showtask>
     </div>
   );
 }
---
+++ src/components/Showtask.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Showtask() {
+    return (
+        <div>Showtask</div>
+    )
+}
+
+export default Showtask
